30a84541119fcc76c196b6df9e3a1d584aef90d6
imdhruv99 <dpdhruvprajapati@gmail.com>
2024-07-31 20:29:56 +0530

    [UPDATE] removed auto increment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    azureId: { type: String, unique: true }, // user.oid
+    oid: { type: String, unique: true }, // user.oid
     name: { type: String, required: true }, // user.displayName
     preferredUsername: { type: String, required: true, unique: true }, // user._json.preferred_username
     createdAt: { type: Date, default: Date.now },
