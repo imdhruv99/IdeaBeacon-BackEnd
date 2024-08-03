5abca6e7bdf2f7f96d7aa118a270db6d8598c0f2
Sanket Patel <patelsanketh98@gmail.com>
2024-08-04 00:54:02 +0530

    [COMPLETE]filter api for stage, category, author, function, subdivision, month and year

--- src/modules/idea/controller.js
+++ src/modules/idea/controller.js
@@ -5,71 +5,119 @@ import { findByOid } from "../user/service.js";
 
 // Create Idea
 export const createIdea = async (req, res) => {
-    try {
-        const user = await findByOid(req.user.oid);
+  try {
+    const user = await findByOid(req.user.oid);
 
-        const newIdea = {
-            ...req.body,
-            createdBy: user._id,
-            updatedBy: user._id
-        };
-        const createdIdea = await ideaService.createIdea(newIdea);
-        res.status(HttpStatusCodes.CREATED.code).json({ status: true, message: responseStrings.createIdeaSuccessMessage, data: createdIdea });
-    } catch (error) {
-        logger.error(`Error creating idea: ${error.message}`);
-        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code).json({ status: false, message: responseStrings.createIdeaErrorMessage });
-    }
+    const newIdea = {
+      ...req.body,
+      createdBy: user._id,
+      updatedBy: user._id,
+    };
+    const createdIdea = await ideaService.createIdea(newIdea);
+    res
+      .status(HttpStatusCodes.CREATED.code)
+      .json({ status: true, message: responseStrings.createIdeaSuccessMessage, data: createdIdea });
+  } catch (error) {
+    logger.error(`Error creating idea: ${error.message}`);
+    res
+      .status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code)
+      .json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  }
 };
 
 // Read All Ideas
 export const getAllIdeas = async (req, res) => {
-    try {
-        const ideas = await ideaService.getAllIdeas();
-        res.status(HttpStatusCodes.OK.code).json({ status: true, data: ideas });
-    } catch (error) {
-        logger.error(`Error fetching ideas: ${error.message}`);
-        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code).json({ status: false, message: responseStrings.createIdeaErrorMessage });
-    }
+  try {
+    const ideas = await ideaService.getAllIdeas();
+    res.status(HttpStatusCodes.OK.code).json({ status: true, data: ideas });
+  } catch (error) {
+    logger.error(`Error fetching ideas: ${error.message}`);
+    res
+      .status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code)
+      .json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  }
 };
 
 // Read Single Idea
 export const getIdeaById = async (req, res) => {
-    try {
-        const idea = await ideaService.getIdeaById(req.params.id);
-        if (!idea) {
-            return res.status(HttpStatusCodes.NOT_FOUND.code).json({ status: false, message: "Idea not found" });
-        }
-        res.status(HttpStatusCodes.OK.code).json({ status: true, data: idea });
-    } catch (error) {
-        logger.error(`Error fetching idea: ${error.message}`);
-        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code).json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  try {
+    const idea = await ideaService.getIdeaById(req.params.id);
+    if (!idea) {
+      return res.status(HttpStatusCodes.NOT_FOUND.code).json({ status: false, message: "Idea not found" });
     }
+    res.status(HttpStatusCodes.OK.code).json({ status: true, data: idea });
+  } catch (error) {
+    logger.error(`Error fetching idea: ${error.message}`);
+    res
+      .status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code)
+      .json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  }
 };
 
 // Update Idea
 export const updateIdea = async (req, res) => {
-    try {
-        const updatedIdea = await ideaService.updateIdea(req.params.id, req.body);
-        if (!updatedIdea) {
-            return res.status(HttpStatusCodes.NOT_FOUND.code).json({ status: false, message: "Idea not found" });
-        }
-        res.status(HttpStatusCodes.OK.code).json({ status: true, data: updatedIdea });
-    } catch (error) {
-        logger.error(`Error updating idea: ${error.message}`);
-        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code).json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  try {
+    const updatedIdea = await ideaService.updateIdea(req.params.id, req.body);
+    if (!updatedIdea) {
+      return res.status(HttpStatusCodes.NOT_FOUND.code).json({ status: false, message: "Idea not found" });
     }
+    res.status(HttpStatusCodes.OK.code).json({ status: true, data: updatedIdea });
+  } catch (error) {
+    logger.error(`Error updating idea: ${error.message}`);
+    res
+      .status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code)
+      .json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  }
 };
 
 // Delete Idea
 export const deleteIdea = async (req, res) => {
-    try {
-        const deletedIdea = await ideaService.deleteIdea(req.params.id);
-        if (!deletedIdea) {
-            return res.status(HttpStatusCodes.NOT_FOUND.code).json({ status: false, message: "Idea not found" });
-        }
-        res.status(HttpStatusCodes.NO_CONTENT.code).json({ status: true, message: "Idea deleted successfully" });
-    } catch (error) {
-        logger.error(`Error deleting idea: ${error.message}`);
-        res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code).json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  try {
+    const deletedIdea = await ideaService.deleteIdea(req.params.id);
+    if (!deletedIdea) {
+      return res.status(HttpStatusCodes.NOT_FOUND.code).json({ status: false, message: "Idea not found" });
     }
+    res.status(HttpStatusCodes.NO_CONTENT.code).json({ status: true, message: "Idea deleted successfully" });
+  } catch (error) {
+    logger.error(`Error deleting idea: ${error.message}`);
+    res
+      .status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code)
+      .json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  }
+};
+
+// Read Filtered Ideas
+export const filterIdeas = async (req, res) => {
+  try {
+    const { stage, category, author, function: func, subdivision, month, year } = req.body;
+
+    let query = {};
+
+    if (stage) query.ideaStageId = stage;
+    if (category) query.ideaCategoryId = category;
+    if (author) query.createdBy = author;
+    if (func) query.functionId = func;
+    if (subdivision) query.subdivisionId = subdivision;
+
+    if (month || year) {
+      query = {
+        ...query,
+        $expr: {
+          $and: [
+            month && { $eq: [{ $month: "$createdAt" }, month] },
+            year && { $eq: [{ $year: "$createdAt" }, year] },
+          ].filter(Boolean),
+        },
+      };
+    }
+
+    // Fetch data from the "ideas" collection
+    const ideas = await ideaService.filteredIdeas(query);
+    res.status(HttpStatusCodes.OK.code).json({ status: true, data: ideas });
+  } catch (error) {
+    logger.error(`Error deleting idea: ${error.message}`);
+    res
+      .status(HttpStatusCodes.INTERNAL_SERVER_ERROR.code)
+      .json({ status: false, message: responseStrings.createIdeaErrorMessage });
+  }
 };
--- src/modules/idea/route.js
+++ src/modules/idea/route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { createIdea, getAllIdeas, getIdeaById, updateIdea, deleteIdea } from "./controller.js";
+import { createIdea, getAllIdeas, getIdeaById, updateIdea, deleteIdea, filterIdeas } from "./controller.js";
 
 const ideaRouter = Router();
 
-ideaRouter.post('/create-idea', createIdea); // Create Idea
-ideaRouter.get('/get-all-idea', getAllIdeas); // Read All Ideas
-ideaRouter.get('/get-idea/:id', getIdeaById); // Read Single Idea
-ideaRouter.put('/update-idea/:id', updateIdea); // Update Idea
-ideaRouter.delete('/delete-idea/:id', deleteIdea); // Delete Idea
+ideaRouter.post("/create-idea", createIdea); // Create Idea
+ideaRouter.get("/get-all-idea", getAllIdeas); // Read All Ideas
+ideaRouter.get("/get-idea/:id", getIdeaById); // Read Single Idea
+ideaRouter.put("/update-idea/:id", updateIdea); // Update Idea
+ideaRouter.delete("/delete-idea/:id", deleteIdea); // Delete Idea
+ideaRouter.post("/filter", filterIdeas);
 
 export default ideaRouter;
