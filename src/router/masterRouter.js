5313c990ad8eefca01d24f7daa6b53cd01e6e4f4
Sanket Patel <patelsanketh98@gmail.com>
2024-08-04 11:20:11 +0530

    [CHANGED]request body params naming and file name changed from route to routes

--- src/router/masterRouter.js
+++ src/router/masterRouter.js
@@ -1,19 +1,18 @@
 import express from "express";
 import authRouter from "../modules/auth/routes.js";
 import { authenticate } from "../modules/common/middleware.js";
-import ideaRouter from "../modules/idea/route.js";
+import ideaRouter from "../modules/idea/routes.js";
 
 const router = express.Router();
 
-
 // Health route
 router.get("/health-check/liveness", async (req, res) => {
-    const healthcheck = {
-      uptime: process.uptime(),
-      message: "OK",
-      timestamp: Date.now(),
-    };
-    res.json({ message: "Welcome to ideaBeacon application.", healthcheck });
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  res.json({ message: "Welcome to ideaBeacon application.", healthcheck });
 });
 router.use("/auth/v1", authRouter);
 
