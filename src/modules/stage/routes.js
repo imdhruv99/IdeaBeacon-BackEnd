c27eaf8e1f64ccce41dfe1afb51834ad5ac7fb22
Sanket Patel <patelsanketh98@gmail.com>
2024-08-04 11:59:11 +0530

    [CHANGED]minor changes

--- src/modules/stage/routes.js
+++ src/modules/stage/routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createIdeaStage, deleteStage, getAllStage, getStageById, updateStage } from "./controller.js";
+import { createIdeaStage, deleteStage, getAllStages, getStageById, updateStage } from "./controller.js";
 import { validateCreateStageRequestBody } from "./middleware.js";
 
 const stageRouter = Router();
 
 stageRouter.post("/create-idea-stage", validateCreateStageRequestBody, createIdeaStage); // Create Stage
-stageRouter.get("/get-all-stage", getAllStage); // Read All Stage
+stageRouter.get("/get-all-stage", getAllStages); // Read All Stage
 stageRouter.get("/get-stage/:id", getStageById); // Read Single Stage
 stageRouter.put("/update-stage/:id", updateStage); // Update Stage
 stageRouter.delete("/delete-stage/:id", deleteStage); // Delete Stage
