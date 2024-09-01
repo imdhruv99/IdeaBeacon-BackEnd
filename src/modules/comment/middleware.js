1fadebb5723e458542d22a6f685fe1284c3e4310
Sanket Patel <patelsanketh98@gmail.com>
2024-09-01 13:40:25 +0530

    [FIXED] Middleware code issue

--- src/modules/comment/middleware.js
+++ src/modules/comment/middleware.js
@@ -2,23 +2,36 @@ import { HttpStatusCodes, responseStrings } from "../../constants/index.js";
 import logger from "../../utils/logger.js";
 import { isEmpty } from "../../utils/utils.js";
 
-// Middleware function for handling pre-processing before handleIdeaCommentController
 export const validateCommentData = (req, res, next) => {
   const createComment = req.body;
-
-  if (
-    isEmpty(createComment) ||
-    isEmpty(createComment.comment) ||
-    isEmpty(createComment.userId) ||
-    isEmpty(createComment.ideaId)
-  ) {
-    logger.error(`Validation failed: Missing required fields in comment data.`);
-    return res.status(HttpStatusCodes.BAD_REQUEST.code).json({
-      status: false,
-      message: responseStrings.missingPayload,
-    });
+  if (!isEmpty(createComment)) {
+    if (createComment.isReply) {
+      if (
+        isEmpty(createComment) ||
+        isEmpty(createComment.replyComment) ||
+        isEmpty(createComment.commentId) ||
+        isEmpty(createComment.userId) ||
+        isEmpty(createComment.ideaId)
+      ) {
+        logger.error(`Validation failed: Missing required fields in comment data.`);
+        return res.status(HttpStatusCodes.BAD_REQUEST.code).json({
+          status: false,
+          message: responseStrings.missingPayload,
+        });
+      }
+    } else if (
+      isEmpty(createComment) ||
+      isEmpty(createComment.comment) ||
+      isEmpty(createComment.userId) ||
+      isEmpty(createComment.ideaId)
+    ) {
+      logger.error(`Validation failed: Missing required fields in comment data.`);
+      return res.status(HttpStatusCodes.BAD_REQUEST.code).json({
+        status: false,
+        message: responseStrings.missingPayload,
+      });
+    }
   }
 
-  logger.info(`Comment data validation successful for idea ID: ${ideaId}`);
   next();
 };
--- src/modules/comment/routes.js
+++ src/modules/comment/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { handleIdeaCommentController } from "./controller.js";
+import { validateCommentData } from "./middleware.js";
 
 const commentRouter = Router();
 
-const commentIdea = [handleIdeaCommentController];
+const commentIdea = [validateCommentData, handleIdeaCommentController];
 commentRouter.post("/idea-comment/:id", commentIdea);
 
 export default commentRouter;
